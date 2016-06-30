193f1056a7e1a84027e1c1d6435ee25c00635577
unknown <E275CEO@KELAL991.kela.root>
2016-06-30 13:26:44 +0300

    kokeile

--- sktipti.R
+++ sktipti.R
@@ -11,3 +11,12 @@ download.file("http://stats.oecd.org/Download.ashx?type=csv&Delimiter=%2c&Includ
 
 download.file("http://statlinks.oecdcode.org/812015091p1g003.xls", # could use direct link I hacked
               destfile="./812015091p1g003.xls")
+
+
+download.file("http://www.lisdatacenter.org/wp-content/uploads/data-key-inequality-workbook.xlsx",
+              destfile = "./data-key-inequality-workbook.xlsx",
+              mode="wb")
+
+d <- readxl::read_excel("./data-key-inequality-workbook.xlsx")
+
+# tehdääs pieni muutos
